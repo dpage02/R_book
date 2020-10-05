8be5557ef637338cd301178e69680eb909c67a7f
Davis Page <davispage@MacBook-Pro.local>
2020-10-05 12:53:50 -0400

    pushing first chapter

---
+++ Comment.R
@@ -0,0 +1,15 @@
+# Script Name: Comment.R
+# Created on: October 5,2020
+# Author: Davis P.
+# Purpose: practice 
+# version: 1.0
+# Execution: must be run as Source to await user input
+
+# Request user input
+name <- readline("Please enter your name: ")
+
+# concatenate input and strings
+greeting <- paste("Welcome", name, "!")
+
+# output concatenated string
+print(greeting)
